c45247b72cdd978238c32d332c082dba0f3f0567
heeyeon3050 <kho200a@naver.com>
2023-04-10 16:43:01 +0900

    내가 좋아하는 사람의 명단을 쉽게 얻을 수 있도록 OneToMany 도입하여 양방향 관계 맺기

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
@@ -30,6 +30,7 @@ public class LikeablePerson {
     @ManyToOne
     private InstaMember fromInstaMember; // 호감을 표시한 사람(인스타 멤버)
     private String fromInstaMemberUsername; // 혹시 몰라서 기록
+
     @ManyToOne
     private InstaMember toInstaMember; // 호감을 받은 사람(인스타 멤버)
     private String toInstaMemberUsername; // 혹시 몰라서 기록
