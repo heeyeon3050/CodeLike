5f8c196ef8aa94b415ad178892fe0363796e7271
heeyeon3050 <kho200a@naver.com>
2023-05-03 14:33:43 +0900

    design: list.html 수정

--- src/main/java/com/ll/gramgram/boundedContext/notification/entity/Notification.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/entity/Notification.java
@@ -10,7 +10,9 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @Getter
@@ -31,4 +33,46 @@ public class Notification extends BaseEntity {
     private int oldAttractiveTypeCode; // 해당사항 없으면 0
     private String newGender; // 해당사항 없으면 null
     private int newAttractiveTypeCode; // 해당사항 없으면 0
+
+    public String getCreateDateStr() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm");
+        return getCreateDate().format(formatter);
+    }
+
+    public String getDifferenceTimeStrHuman() {
+
+        //두 시간 사이의 차이 계산
+        Duration duration = Duration.between(getCreateDate(), LocalDateTime.now());
+
+        //올림을 적용한 시간 차이 계산
+        long hours = duration.toHours();
+        long minutes = duration.toMinutes() % 60;
+        long seconds = duration.toSeconds() % 60;
+
+        // 초가 남았을 경우 분에 1을 더함
+        if (seconds > 0) {
+            minutes += 1;
+        }
+
+        // 분이 60일 경우 시간에 1을 더하고 분을 0으로 초기화
+        if (minutes == 60) {
+            hours += 1;
+            minutes = 0;
+        }
+
+        //0시간일 경우 분만 출력
+        if (hours == 0) {
+            return minutes + "분";
+        }
+
+        return hours + "시간 " + minutes + "분";
+    }
+
+    public String getAttractiveTypeDisplayName(int attractiveTypeCode) {
+        return switch (attractiveTypeCode) {
+            case 1 -> "외모";
+            case 2 -> "성격";
+            default -> "능력";
+        };
+    }
 }
